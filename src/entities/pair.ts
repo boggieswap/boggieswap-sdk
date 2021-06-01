7f13cd5bda1547c73d4375601cc22b5bcfe76ee1
boggieswap <83878077+boggieswap@users.noreply.github.com>
2021-06-01 16:21:47 +0545

    Update pair.ts

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -55,8 +55,8 @@ export class Pair {
       tokenAmounts[0].token.chainId,
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token),
       18,
-      'APESWAP-LP',
-      'ApeSwap V2'
+      'BOGGIESWAP-LP',
+      'BoggieSwap V2'
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]
   }
